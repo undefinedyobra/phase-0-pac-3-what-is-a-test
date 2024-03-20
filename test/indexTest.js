3b580eeac3038f0afeb0fae8bfff09651671f627
undefinedyobra <brianmelly008@gmail.com>
2024-03-20 13:51:00 +0300

    what is a test

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,6 +2,14 @@ require ( './root.js' );
 
 const { name, height, message } = require("../index.js");
 
+describe("Name", () => {
+  it('returns "Melly"', () => {
+    expect(name).toEqual("Melly");
+  });
+});
+
+
+
 /*
 describe('what-is-a-test', () => {
   describe('Name', () => {
@@ -9,6 +17,26 @@ describe('what-is-a-test', () => {
       expect(name).toEqual('Susan')
     })
   })
+
+
+
+
+*/describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
+
+
+
+
+describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
+https://moringa.instructure.com/courses/629/assignments/44660?module_item_id=94060#:~:text=describe(%22Height%22%2C%20()%20%3D%3E%20%7B%0A%20%20it(%22is%20less%20than%2040%22%2C%20()%20%3D%3E%20%7B%0A%20%20%20%20expect(height).toBeLessThan(40)%3B%0A%20%20%7D)%3B%0A%7D)%3B
+
 
 
   describe('Height', () => {
